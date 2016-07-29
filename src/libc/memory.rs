a7e2d2fabeee1d3a2fcb4001d42b08f69abc127b
Zach Marcantel <zmarcantel@gmail.com>
2016-07-29 02:06:37 -0500

    add memcpy

--- src/libc/memory.rs
+++ src/libc/memory.rs
@@ -11,6 +11,7 @@ use core::cmp::{PartialOrd, PartialEq};
 /// Set a region of memory to a given byte value.
 ///
 /// Internally, this uses compiler intrinsics (volatile_set_memory).
+///
 pub unsafe fn memset(dest: *mut u8, val: u8, cnt: usize) {
     // TODO: negative byte safety -- compiler seems to nop it
     intrinsics::volatile_set_memory(dest, val, cnt);
@@ -74,6 +75,7 @@ macro_rules! lex_cmp {
 ///
 /// Returns the lexical ordering of A compared to B.
 /// Thus, all possible returns are [-1, 0, 1].
+///
 pub unsafe fn memcmp<T>(a: *mut u8, b: *mut u8, byte_cnt: usize)
     -> i8
     where T: PartialEq + PartialOrd
@@ -196,6 +198,64 @@ mod memcmp {
         unsafe {
             super::memset(buff.as_mut_ptr(), 0xAF, TEST_LEN);
             assert_eq!(0, super::memcmp::<u64>(buff.as_mut_ptr(), expect.as_mut_ptr(), TEST_LEN));
+        }
+    }
+}
+
+
+//------------------------------------------------
+//
+// memcpy
+//
+//------------------------------------------------
+
+/// Copies one region of memory to another.
+///
+/// Based on the addresses given and the number of bytes, this function
+/// will delegate to one of the following intrinsics:
+///
+/// * volatile_copy_memory
+/// * volatile_copy_nonoverlapping_memory
+///
+pub unsafe fn memcpy(dst: *mut u8, src: *mut u8, byte_cnt: usize) {
+    if (dst < src && dst.offset(byte_cnt as isize) > src) || (src < dst && src.offset(byte_cnt as isize) > dst) {
+        intrinsics::volatile_copy_memory(dst, src, byte_cnt);
+    } else {
+        intrinsics::volatile_copy_nonoverlapping_memory(dst, src, byte_cnt);
+    }
+}
+#[cfg(test)]
+mod memcpy {
+    #[test]
+    fn non_overlapping() {
+        const TEST_LEN: usize = 64;
+        let mut buff: [u8; TEST_LEN] = [0; TEST_LEN];
+        let mut expect: [u8; TEST_LEN] = [0xAF; TEST_LEN];
+        unsafe {
+            super::memcpy(buff.as_mut_ptr(), expect.as_mut_ptr(), TEST_LEN);
+            assert_eq!(0, super::memcmp::<u8>(buff.as_mut_ptr(), expect.as_mut_ptr(), TEST_LEN));
+        }
+    }
+
+    #[test]
+    fn overlapping() {
+        const TEST_LEN: usize = 64;
+        const HALF_TEST_LEN: usize = TEST_LEN/2;
+        let mut buff: [u8; TEST_LEN] = [0xB4; TEST_LEN];
+        unsafe {
+            super::memset(buff.as_mut_ptr().offset(HALF_TEST_LEN as isize), 0xAF, HALF_TEST_LEN);
+
+            // we will now copy the first half into the second half, but shifted back `distance` bytes
+            let distance = HALF_TEST_LEN / 2;
+            let overlap = (HALF_TEST_LEN)-distance;
+            super::memcpy(buff.as_mut_ptr(), buff.as_mut_ptr().offset(overlap as isize), HALF_TEST_LEN);
+
+            for i in 0..distance {
+                assert_eq!(0xB4, buff[i]);
+            }
+            for i in distance..TEST_LEN {
+                assert_eq!(0xAF, buff[i]);
+            }
         }
     }
 }
