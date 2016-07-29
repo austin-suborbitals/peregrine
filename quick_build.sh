3771dd851e9cd4dfac24a3eb1becccbec57a4ee7
Zach Marcantel <zmarcantel@gmail.com>
2016-07-28 23:11:09 -0500

    let travis use quick_build

--- quick_build.sh
+++ quick_build.sh
@@ -1,3 +1,10 @@
-PRGTARGET=thumbv7em-none-eabi
+if [ -z "$PRGTARGET" ]; then
+    export PRGTARGET=thumbv7em-none-eabi
+fi
 
-RUST_TARGET_PATH=$RUST_TARGET_PATH:./build_targets cargo build --verbose --target $PRGTARGET
+if [ "$PRGTARGET" == "host" ] ; then
+    cargo test --verbose
+else
+    export RUST_TARGET_PATH=$RUST_TARGET_PATH:./build_targets
+    cargo build --verbose --target $PRGTARGET
+fi
