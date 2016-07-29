9b2cb0e4a527ba9e91a67583af874ef6d85ee2f6
Zach Marcantel <zmarcantel@gmail.com>
2016-07-29 00:17:53 -0500

    move testing target name to test from host

--- quick_build.sh
+++ quick_build.sh
@@ -4,7 +4,7 @@ if [ -z "$PRGTARGET" ]; then
     export PRGTARGET=$PRGDEFAULT
 fi
 
-if [ "$PRGTARGET" == "host" ] ; then
+if [ "$PRGTARGET" == "test" ] ; then
     cargo test --verbose
 else
     export RUST_TARGET_PATH=$RUST_TARGET_PATH:./build_targets
